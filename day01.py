164cef05264c6655b0118d787da9533a7f0c8974
Ammon Whatcott <ammonwhatcott@gmail.com>
2022-12-04 16:41:55 -0700

    saved answers

--- day01.py
+++ day01.py
@@ -1,15 +1,17 @@
-file=open("day01.txt") 
+file = open("day01.txt") 
 
-elf=0
-elves=[]
+elf = 0
+elves = []
+
 for line in file:
-    line=line.strip()
+    line = line.strip()
     if not line:
         elves.append(elf)
-        elf=0
+        elf = 0
     else:
-        elf=elf+int(line)
+        elf = elf + int(line)
+
 elves.append(elf)
 elves=sorted(elves,reverse=True)
-print(elves[0])
-print(sum(elves[0:3]))
+print(elves[0])        #  68442
+print(sum(elves[0:3])) # 204837
